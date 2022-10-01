eb8c8dac2bbf921a89fe38e2ea9f724a654749aa
gandhyman <gandhycoder@gmail.com>
2022-10-01 11:53:51 -0600

    basic working

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
 const express = require('express');
 const path = require('path');
+// const pug = require('pug');
 
 let app = express();
 app.set('port', 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', pug);
+app.set('view engine', 'pug');
 
 const posts = [
   {
     handle: 'el-blog-mas-rapido',
-    titulo: 'El bolg mas rapido',
+    titulo: 'El blog mas rapido',
     description: 'En este post te muestro como hacer tu blog rapido',
   },
   {
     handle: 'seo-para-tu-blog',
-    titulo: 'Como mejorar el SEO de tu blog',
+    titulo: 'SEO de tu blog',
     description: 'En este post te muestro como hacer el mejor SEO',
   },
   {
     handle: 'analiticas-para-tu-blog',
-    titulo: 'Como instalar y trackear a tu audiencia',
+    titulo: 'Analiticas para tu blog',
     description:
       'En este post te muestro como trackear lo que tu audiencia lee',
   },
 ];
 
-app.get('/', (req, res) => res.render('home'));
+app.get('/', (req, res) => res.render('home', { posts }));
+app.get('/posts/:handle', (req, res) => {
+  res.render(`posts/${req.params.handle}`);
+});
 
 app.listen(app.get('port'), function () {
   console.log('Listening in port %d', app.get('port'));
